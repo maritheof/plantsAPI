c99fbafae2184b1c422af6849e84f49887881b0a
maritheof <101402217+maritheof@users.noreply.github.com>
2022-11-04 21:20:24 +0200

    Delete PlantRepository.java

--- PlantRepository.java
+++
@@ -1,9 +0,0 @@
-package demo.repository;
-
-import demo.model.Plant;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlantRepository extends JpaRepository<Plant, Long> {
-
-}
